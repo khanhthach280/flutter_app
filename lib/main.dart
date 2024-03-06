af7165a0d5117ab9cd231c8c2e6bb796473c1622
khanh thach <thach.khanh@izota.vn>
2024-03-06 08:53:02 +0700

    add api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application/next_page.dart';
+// ignore: depend_on_referenced_packages
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -41,8 +45,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    // fetchData();
+  }
 
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://8241e79b-0f84-447b-a817-4f2782c17227.mock.pstmn.io/fruits'));
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print('A network error occurred');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -52,12 +72,26 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(onPressed: () {
-                          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MyWidget()),
-            );
-            }, child: Text('Move to next page')),
+            ElevatedButton(
+                onPressed: () async {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => MyWidget()),
+                  // );
+
+                  try {
+                    final response = await http.get(Uri.parse('https://8241e79b-0f84-447b-a817-4f2782c17227.mock.pstmn.io/fruits'));
+
+                    if (response.statusCode == 200) {
+                      print(response.body);
+                    } else {
+                      print('A network error occurred');
+                    }
+                  } catch (error) {
+                    print('An error occurred: $error');
+                  }
+                },
+                child: Text('Move to next page')),
             const Text(
               'You have pushed the button this many times:',
             ),
